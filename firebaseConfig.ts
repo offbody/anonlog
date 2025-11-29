ae0d3c29a8527058dd61f082bf473f5631d5d9a1
offbody <82654492+offbody@users.noreply.github.com>
2025-11-29 13:41:44 +0300

    Add files via upload

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,7 +1,8 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // TODO: Вставь сюда данные из консоли Firebase (Project Settings -> Your apps -> SDK setup and configuration)
 const firebaseConfig = {
@@ -17,3 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
